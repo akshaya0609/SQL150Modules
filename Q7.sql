673cf04a00204efa75411e0c90bca67c0d71e3c1
akshaya0609 <akshayap0609@gmail.com>
2025-08-30 15:12:30 -0500

    Deleted

--- Q7.sql
+++
@@ -1,6 +0,0 @@
-SELECT host_id, round(avg(rating),2) as avrg from  listings l
-join reviews r on l.listing_id=r.listing_id
-group by host_id
-having count(distinct l.listing_id)>=2
-order by avrg desc
-limit 2;
